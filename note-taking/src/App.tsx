952a6feea443aef662e7f2d217331d8b1d80388d
Ahmad Akel <ahmad.akel@mgm-tp.com>
2023-05-19 01:19:21 +0200

    Add NoteForm & NewForm Components

--- note-taking/src/App.tsx
+++ note-taking/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
-import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./components/NewNote";
+import { Container } from "react-bootstrap";
 function App() {
-  return <div className="App">Note Taking</div>;
+  return (
+    <Container className="md-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="/:id/edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
